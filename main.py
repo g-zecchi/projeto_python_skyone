3037bf5f41d2093f9a24ba5fbc9e0fb7d47f5bcb
Gabriel Zecchi <g.zecchi@hotmail.com>
2022-06-13 15:01:39 -0300

    Lógica criação devs

--- main.py
+++ main.py
@@ -45,6 +45,17 @@ while True:
 
     squads.append(squad)
 
+    while True:
+        nome_dev = input('\nNome do desenvolvedor: ')
+        fone_dev = input('Telefone do desenvolvedor: ')
+        cargo_dev = input('Cargo do desenvolvedor: ')
+        dev = Dev(nome_dev, fone_dev, cargo_dev)
+        dev.incluir_squad(squad)
+        squad.incluir_dev(dev)
+        option = input('\nDeseja adicionar mais um dev [S/N]: ')
+        if option in 'Nn':
+            break
+
     option = input('\nDeseja adicionar mais uma squad [S/N]: ')
     if option in 'Nn':
         break
